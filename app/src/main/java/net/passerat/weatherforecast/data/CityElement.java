292ecd293d13a817702e1d7a3792776cc4c21500
Boris Passerat de La Chapelle <bpasserat@h2ad.net>
2017-09-25 21:33:20 +0200

    Ajout de documentation sur les données du programme

--- app/src/main/java/net/passerat/weatherforecast/data/CityElement.java
+++ app/src/main/java/net/passerat/weatherforecast/data/CityElement.java
@@ -2,19 +2,34 @@ package net.passerat.weatherforecast.data;
 
 /**
  * Created by bpasserat on 22/09/2017.
+ *
+ * This class implement the information to describe a city element
+ *
  */
 
 public class CityElement {
 
+    /**
+     * Constructor for the CityElement class
+     * @param name the city name
+     * @param latitude the latitude  of the city
+     * @param longitude  the longitude of the city
+     */
     public CityElement(String name , float latitude , float longitude) {
         this.name = name ;
         this.latitude = latitude ;
         this.longitude = longitude ;
     }
 
+
     public float longitude ;
     public float latitude ;
     public String name ;
+
+    /**
+     *
+     * @return String with the name of the city
+     */
 
     public String toString() {
         return name;
--- app/src/main/java/net/passerat/weatherforecast/data/WeatherElement.java
+++ app/src/main/java/net/passerat/weatherforecast/data/WeatherElement.java
@@ -5,10 +5,16 @@ import java.util.Date;
 
 /**
  * Created by bpasserat on 21/09/2017.
+ *
+ * This class describe a weather element
+ *
  */
 
 public class WeatherElement implements Serializable{
 
+    /**
+     *  This enum gives the differents state of the sky
+     */
     public enum WeatherState  {
          SUN  ,
         CLOUD ,
@@ -17,16 +23,53 @@ public class WeatherElement implements Serializable{
         THUNDER
     }
 
-
+    /**
+     *  Weather sky state
+     */
     public WeatherState weatherState ;
+
+
+    /**
+     *  Temperature in °C
+     */
     public float temperatureCelsius ;
+
+    /**
+     *  Pressure in Pascal
+     */
     public float pressurePa ;
+
+    /**
+     *  Wind speed km/h
+     */
     public float windSpeedKmHour ;
+
+    /**
+     * Wind Orientation
+     */
     public float windOrientationDegree ;
+
+    /**
+     * Date for the forecast
+     */
     public Date date ;
+
+    /**
+     * City for the forecast
+     */
     public String city ;
 
 
+    /**
+     *  Constructor for a city element
+     * @param city city name where this weather
+     * @param date date of the forecast
+     * @param weatherState state of the sky
+     * @param temperatureCelsius Temperature in °C
+     * @param pressurePa Pressure in Pascal
+     * @param windSpeedKmHour Wind speed in km/h
+     * @param windOrientationDegree Wind orientation in km/h
+     */
     public WeatherElement( String  city , Date date , WeatherState weatherState , float temperatureCelsius , float pressurePa ,
                            float windSpeedKmHour , float windOrientationDegree ) {
         this.city = city ;
