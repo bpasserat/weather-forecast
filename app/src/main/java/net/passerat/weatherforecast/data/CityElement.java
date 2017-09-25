c219d8e1c477a4968cd85afde79ac8d96bc056b9
Boris Passerat de La Chapelle <bpasserat@h2ad.net>
2017-09-25 22:33:32 +0200

    Ajout du chargement à partir de JSONObject et de la tranformation vers JSONObject

--- app/src/main/java/net/passerat/weatherforecast/data/CityElement.java
+++ app/src/main/java/net/passerat/weatherforecast/data/CityElement.java
@@ -1,5 +1,8 @@
 package net.passerat.weatherforecast.data;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by bpasserat on 22/09/2017.
  *
@@ -8,6 +11,10 @@ package net.passerat.weatherforecast.data;
  */
 
 public class CityElement {
+
+    private static final String KEY_NAME = "name" ;
+    private static final String KEY_LONGITUDE = "longitude" ;
+    private static final String KEY_LATITUDE = "latitude" ;
 
     /**
      * Constructor for the CityElement class
@@ -21,17 +28,35 @@ public class CityElement {
         this.longitude = longitude ;
     }
 
+    /**
+     * Constructor from a JSONObject that can be received from the network !
+     * @param obj JSONObject matching to a CityElement
+     * @throws Exception If the object do not have all properties
+     */
+    public CityElement(JSONObject obj) throws  JSONException {
+        this.name = obj.getString(KEY_NAME);
+        this.longitude = (float)obj.getDouble(KEY_LONGITUDE);
+        this.latitude = (float)obj.getDouble(KEY_LATITUDE);
+    }
 
     public float longitude ;
     public float latitude ;
     public String name ;
 
     /**
-     *
-     * @return String with the name of the city
+     *  Convert a CityElement to a JSONObject
+     * @return JSONObject
+     * @throws JSONException
      */
-
     public String toString() {
         return name;
+    }
+
+    public JSONObject toJSON() throws JSONException{
+        JSONObject obj = new JSONObject() ;
+        obj.put(KEY_NAME , this.name);
+        obj.put(KEY_LATITUDE , (double)this.latitude) ;
+        obj.put(KEY_LONGITUDE , (double)this.longitude);
+        return obj;
     }
 }
--- app/src/main/java/net/passerat/weatherforecast/data/WeatherElement.java
+++ app/src/main/java/net/passerat/weatherforecast/data/WeatherElement.java
@@ -1,5 +1,8 @@
 package net.passerat.weatherforecast.data;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -16,7 +19,7 @@ public class WeatherElement implements Serializable{
      *  This enum gives the differents state of the sky
      */
     public enum WeatherState  {
-         SUN  ,
+        SUN  ,
         CLOUD ,
         RAIN  ,
         SNOW ,
@@ -81,12 +84,94 @@ public class WeatherElement implements Serializable{
         this.windOrientationDegree = windOrientationDegree ;
     }
 
+    private static final String KEY_CITY = "city" ;
+    private static final String KEY_STATE = "state" ;
+    private static final String KEY_PRESSURE = "pressure" ;
+    private static final String KEY_TEMPERATURE = "temperature" ;
+    private static final String KEY_WIND_SPEED = "windSpeed" ;
+    private static final String KEY_WIND_ORIENTATION = "windOrientation" ;
+
+    /**
+     * Constructor from a JSONObject that can be received from the network !
+     * @param obj JSONObject matching to a WeatherElement
+     * @throws Exception If the object do not have all properties
+     */
+    public WeatherElement(JSONObject obj) throws  Exception {
+        this.city = obj.getString("city");
+        int state = obj.getInt("state");
+        switch (state) {
+            case 0 :
+                this.weatherState = WeatherState.SUN;
+                break;
+            case 1 :
+                this.weatherState = WeatherState.THUNDER;
+                break;
+            case 2 :
+                this.weatherState = WeatherState.CLOUD;
+                break ;
+            case 3 :
+                this.weatherState = WeatherState.RAIN;
+                break;
+            case 4 :
+                 this.weatherState = WeatherState.SNOW;
+                 break ;
+            default:
+                throw new Exception("Unknown weather state") ;
+        }
+        this.pressurePa = (float) obj.getDouble("pressure");
+        this.temperatureCelsius = (float) obj.getDouble("temperature");
+        this.windSpeedKmHour = (float) obj.getDouble("windSpeed");
+        this.windOrientationDegree = (float) obj.getDouble("windOrientation");
+    }
+
+    private int getWeatherStateIntValue( WeatherState state) {
+        switch (state) {
+            case SUN:
+                return 0 ;
+            case CLOUD:
+                return 2 ;
+            case RAIN:
+                return 3;
+            case SNOW:
+                return 4;
+            case THUNDER:
+                return 1;
+        }
+        return -1;
+    }
+
+    /**
+     *  Convert a WeatherElement to a JSONObject
+     * @return JSONObject
+     * @throws JSONException
+     */
+    public JSONObject toJSON() throws JSONException {
+        JSONObject obj = new JSONObject( ) ;
+        obj.put(KEY_CITY , this.city );
+        obj.put(KEY_TEMPERATURE , this.temperatureCelsius );
+        obj.put(KEY_PRESSURE , this.pressurePa );
+        obj.put(KEY_STATE , getWeatherStateIntValue(this.weatherState) );
+        obj.put(KEY_WIND_ORIENTATION , this.windOrientationDegree);
+        obj.put(KEY_WIND_SPEED , this.windSpeedKmHour );
+        return obj;
+    }
+
+    /**
+     * Convert temperature from °C to °F
+     * @return temperature in °F
+     */
     public float temperatureToFarenheit( ) {
         return 1.8f*this.temperatureCelsius + 32 ;
     }
 
+    /**
+     * Convert Wind Speed from km/h to miles/h
+     * @return wind speed in miles/h
+     */
     public float windSpeedToMilePerHour() {
         return this.windSpeedKmHour / 1.6034f ;
     }
+
+
 
 }
